0f80ee62382d1873683421ab81e5d43d75d3996a
Masterz <2470071252@qq.com>
2022-10-06 18:23:24 +0800

    none

--- src/cn/tedu/eliminate/Element.java
+++ src/cn/tedu/eliminate/Element.java
@@ -23,6 +23,22 @@ public abstract class Element {
         eliminatedIndex = 0;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     /**
      * 是否是选中的
      * @return
@@ -78,6 +94,7 @@ public abstract class Element {
             this.getImage().paintIcon(null,graphics,this.x,this.y);
         }
     }
+
 
 
 }
--- src/cn/tedu/eliminate/World.java
+++ src/cn/tedu/eliminate/World.java
@@ -93,9 +93,74 @@ public class World extends JPanel {
                 do {
                     Element e = createElement(row, col); // 创建元素
                     elements[row][col] = e; // 将元素填充到elements数组中
-                }while (canEliminate(row,col)!=ELIMINATE_NON); // 若可消除则重新生成元素
+                } while (canEliminate(row, col) != ELIMINATE_NON); // 若可消除则重新生成元素
             }
         }
+    }
+
+    /**
+     * 判断两个元素是否能交换（相邻则能交换，否则不能交换）
+     */
+    public boolean isAdjacent() {
+        // 若行相邻并且列相等，或，若列相邻并且行相等
+        if ((Math.abs(firstRow - secondRow) == 1 && firstCol == secondCol)
+                || (Math.abs(firstCol - secondCol) == 1 && firstRow == secondRow)) {
+            return true;  // 相邻
+        } else {
+            return false;  // 不相邻
+        }
+    }
+
+    /**
+     * 移动两个元素
+     */
+    public void moveElement(){
+        if (firstRow==secondRow){ // 若行号相同，表示左右动
+            int firstX = OFFSET+firstCol*ELEMENT_SIZE; // 第一个元素的x坐标
+            int secondX = OFFSET+secondCol*ELEMENT_SIZE; // 第二个元素的x坐标
+            int step = firstX<secondX?4:-4;  // 步长(控制左右)，值越大速度越快
+            for (int i=0 ;i<15;i++){ // 走15次（每次走4，共15*4为60）
+                try{ // 每动一下休眠10ms
+                    Thread.sleep(10);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                firstX+=step; // firstX加
+                secondX-=step; // secondX减
+                // 修改元素坐标（刚刚只是在修改变量的值，一定是元素坐标修改之后才能看到移动）
+                elements[firstRow][firstCol].setX(firstX);
+                elements[secondRow][secondCol].setX(secondX);
+                repaint();
+            }
+        }
+        if (firstCol==secondCol) { // 若列号相同表示上下动
+            int firstY = OFFSET+firstRow*ELEMENT_SIZE;
+            int secondY = OFFSET+secondRow*ELEMENT_SIZE;
+            int step = firstY<secondY?4:-4;
+            for (int i=0;i<15;i++){
+                try {
+                    Thread.sleep(10);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                firstY+=step;
+                secondY-=step;
+                elements[firstRow][firstCol].setY(firstY);
+                elements[secondRow][secondCol].setY(secondY);
+                repaint();
+            }
+        }
+    }
+
+    /**
+     * 交换两个元素
+     */
+    public void exchangeElement(){
+        Element e1 = elements[firstRow][firstCol]; // 获取第一个选中元素
+        Element e2 = elements[secondRow][secondCol]; // 获取第二个选中元素
+        elements[firstRow][firstCol] = e2; // 将第一个选中元素修改为第二个元素
+        elements[secondRow][secondCol] = e1; // 将第二个选中的元素修改为第一个元素
+
     }
 
     private boolean canInteractive = true; // 可交互状态(默认为true)
@@ -103,7 +168,7 @@ public class World extends JPanel {
     private int firstRow = 0; // 第一个选中元素ROW
     private int firstCol = 0; // 第一个选中元素COL
     private int secondRow = 0; // 第二个选中元素ROW
-    private int secondCol =0; // 第二个选中元素COL
+    private int secondCol = 0; // 第二个选中元素COL
 
     /**
      * 启动执行程序
@@ -121,12 +186,39 @@ public class World extends JPanel {
                 // 若不可交互时，不响应鼠标事件
                 if (!canInteractive) return;
                 canInteractive = false; // 只要选中元素，就先将状态修改为不可交互
+                int row = (e.getY() - OFFSET) / ELEMENT_SIZE; // 获取选中元素的row
+                int col = (e.getX() - OFFSET) / ELEMENT_SIZE; // 获取选中元素的col
                 selectedNumber++; // 表示选中元素个数增1
-                if (selectedNumber==1){ //第一次选中
-                    //...
+                if (selectedNumber == 1) { //第一次选中
+                    firstRow = row;  // 记录第一个元素row
+                    firstCol = col;  // 记录第一个元素col
+                    elements[firstRow][firstCol].setSelected(true); // 设置选中状态为true
                     canInteractive = true; // 可交互
-                } else if (selectedNumber==2) { // 第二次选中
-                    //...
+
+                } else if (selectedNumber == 2) { // 第二次选中
+                    secondRow = row; // 记录第2个元素row
+                    secondCol = col; // 记录第2个元素col
+                    elements[secondRow][secondCol].setSelected(true);
+                    if (isAdjacent()){ // 若相邻
+                        new Thread(){
+                            public void run(){
+                                elements[firstRow][firstCol].setSelected(false);
+                                elements[secondRow][secondCol].setSelected(false);
+                                // 交换、消除
+                                moveElement();  // 移动两个元素(移动动画)
+                                exchangeElement(); // 交换两个元素
+                                canInteractive = true; // 可交互
+
+
+                            }
+                        }.start();
+
+                    }else {  // 不相邻
+                        elements[firstRow][firstCol].setSelected(false);  // 取消选中状态
+                        elements[secondRow][secondCol].setSelected(false); // 取消选中状态
+                        canInteractive = true;
+                    }
+
                     canInteractive = true; //可交互(在某种条件下才变为可交互)
                     selectedNumber = 0; //选中个数归零
                 }
