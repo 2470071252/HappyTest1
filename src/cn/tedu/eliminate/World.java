3b5e6802e577f247741a738f1f3abcb062b55882
Masterz <2470071252@qq.com>
2022-10-06 13:08:47 +0800

    none

--- src/cn/tedu/eliminate/World.java
+++ src/cn/tedu/eliminate/World.java
@@ -2,6 +2,7 @@ package cn.tedu.eliminate;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class World extends JPanel {
     public static final int WIDTH =429; // 窗口的宽度
@@ -12,18 +13,40 @@ public class World extends JPanel {
     public static final int OFFSET = 30; // 偏移量（第一个元素距离窗口左边缘的距离）
     private Element[][] elements = new Element[ROWS][COLS]; // 元素数组 （8行6列）
 
+    // 元素类型
+    public static final int ELEMENT_TYPE_BEAR = 0;  // 熊
+    public static final int ELEMENT_TYPE_BIRD = 1; // 鸟
+    public static final int ELEMENT_TYPE_FOX = 2;  // 狐狸
+    public static final int ELEMENT_TYPE_FROG = 3; // 青蛙
+
     /**
      * 创建元素(根据行列来计算x/y坐标，而后创建对象)
+     *
      * @param row
      * @param col
-     * @return
      */
     public Element createElement(int row, int col){
         int x = OFFSET+col*ELEMENT_SIZE; // 列col的值控制x坐标
         int y = OFFSET+row*ELEMENT_SIZE; // 行row的值控制y坐标
-
-
-        return null;
+        /*
+        假设：
+        1）row=0 col=0 x=30 y=30
+        2）row=0 col=1 x=90 y=30
+        3）row=0 clo=2 x=150 y=30
+        ……
+         */
+        Random random = new Random(); // 随机数对象
+        int type = random.nextInt(4); // 0-3之间
+        switch (type){ // 根据type不同来生成不同的对象
+            case ELEMENT_TYPE_BEAR:
+                return new Bear(x,y);
+            case ELEMENT_TYPE_BIRD:
+                return new Bird(x,y);
+            case ELEMENT_TYPE_FOX:
+                return new Fox(x,y);
+            default:
+                return new Frog(x,y);
+        }
     }
 
     /**
@@ -67,6 +90,6 @@ public class World extends JPanel {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        world.start();
+        world.start();  // 启动程序的执行
     }
 }
